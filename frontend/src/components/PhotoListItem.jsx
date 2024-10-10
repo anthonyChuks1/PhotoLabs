57ab15c1a9d3c0a988ad18d3dd0aa4f894d24766
anthonyChuks1 <anuebunwaanthony1@gmail.com>
2024-10-10 11:50:58 -0400

    fixed the css implementation for PhotoListItem

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -8,14 +8,18 @@ import "../styles/PhotoListItem.scss";
 const PhotoListItem = ({photo}) => {
   /* Insert React */
   const {imageSource, profile, username, location:{city, country}} = photo
-  return <div className = "photo-list__item">
-    <img src = {imageSource} className="photo-list__image"></img>
-    <img src = {profile} className = "photo-list__user-profile"></img>
-    <div className = "photo-list__user-details">
-    <p className = "photo-list__user-info">{username}</p>
-    <p className = "photo-list__user-location">{city}, {country}</p>
+  return (
+    <div className = "photo-list__item">
+      <img src = {imageSource} className="photo-list__image"></img>
+      <div className = "photo-list__user-details">
+        <img src = {profile} className = "photo-list__user-profile"></img>
+        <div className = "photo-list__user-info">
+          <p>{username}</p>
+          <p className = "photo-list__user-location">{city}, {country}</p>
+        </div>
+      </div>
     </div>
-  </div>
+  );
 };
 
 export default PhotoListItem;
