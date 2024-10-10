5e06f0fbed2b4d8720869a1843a3970f2c4385a8
anthonyChuks1 <anuebunwaanthony1@gmail.com>
2024-10-10 19:07:23 -0400

    Built the built the photolist component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,33 +3,26 @@ import React from "react";
 import PhotoListItem from "./components/PhotoListItem";
 import PhotoFavButton from "components/PhotoFavButton";
 import "./App.scss";
+import PhotoList from "components/PhotoList";
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
 
-const photos = [
-  sampleDataForPhotoListItem,
-  sampleDataForPhotoListItem,
-  sampleDataForPhotoListItem,
-];
+
+// const photos = [
+//   sampleDataForPhotoListItem,
+//   sampleDataForPhotoListItem,
+//   sampleDataForPhotoListItem,
+// ];
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
-      {photos.map((photo, index) => (
+      {/* {photos.map((photo, index) => (
         <div key={index} className="photo-list__fav-icon-svg">
           <PhotoListItem photo={photo}></PhotoListItem>
         </div>
-      ))}
+      ))} */}
+      <PhotoList/>
     </div>
   );
 };
--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import "../styles/PhotoList.scss";
+import PhotoListItem from "./PhotoListItem";
 
 const sampleDataForPhotoList = [
   {
@@ -54,12 +55,27 @@ const sampleDataForPhotoList = [
       profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
     },
   },
+  {
+    id: "4",
+    location: {
+      city: "Montreal",
+      country: "Canada",
+    },
+    imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+    username: "Joe Example",
+    profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+  }
 ];
 
 const PhotoList = () => {
   return (
     <ul className="photo-list">
       {/* Insert React */}
+      {sampleDataForPhotoList.map((photo) => (
+        <div key={photo.id} className="photo-list__fav-icon-svg">
+          <PhotoListItem photo={photo}></PhotoListItem>
+        </div>
+      ))}
     </ul>
   );
 };
