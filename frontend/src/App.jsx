04d0cf73c2bdad519bdc977000411cc4ca2ba1f9
anthonyChuks1 <anuebunwaanthony1@gmail.com>
2024-10-12 06:02:13 -0400

    This is meant to fail

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,18 @@
 import React, { useState } from "react";
 import "./App.scss";
 import HomeRoute from "routes/HomeRoute";
+import "./styles/PhotoDetailsModal.scss"
 import photos from "mocks/photos";
 import topics from "mocks/topics";
 // Note: Rendering a single component to build components in isolation
+
+
 const App = () => {
+  //The array that holds the id for the favorite photographs
   const [favPhotos, setFavPhotos] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false)
   //const [favFlag, setFavFlag] = useState(false);
+  //handles the array that keeps track of the favorite photo 
   const handleFavList = (selected, photo) => {
     if (selected) {
       setFavPhotos((prevFavPhotos) => {
@@ -25,18 +31,26 @@ const App = () => {
       });
     }
   };
+
+  //handles the favbadge icon on the nav bar 
   const handleFavListFlag = () => {
     return favPhotos.length ? true : false;
   };
-
+  
+  const handleModal = () => {
+    const isModalOpenNew = !isModalOpen;
+    setIsModalOpen(isModalOpenNew);
+    console.log("Image clicked");
+  }
   return (
     <div className="App">
       <HomeRoute
         topics={topics}
         photos={photos}
         handleFavList={handleFavList}
-        favFlag={handleFavListFlag()}
-      />
+        isFavPhotoExist={handleFavListFlag()}
+        handleModal = {handleModal}      
+      > {isModalOpen && <alert> Meant to contain an image</alert>}</HomeRoute>
     </div>
   );
 };
