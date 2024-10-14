22047ca7e717835844e1f038c2efd1492287262c
anthonyChuks1 <anuebunwaanthony1@gmail.com>
2024-10-14 15:36:15 -0400

    moved all state and state handlers to useApplicationData

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,44 +5,13 @@ import "./styles/PhotoDetailsModal.scss";
 import photos from "mocks/photos";
 import topics from "mocks/topics";
 import PhotoDetailsModal from "routes/PhotoDetailsModal";
+import useApplicationData from "./hooks/useApplicationData";
 // Note: Rendering a single component to build components in isolation
 
 const App = () => {
   //The array that holds the id for the favorite photographs
-  const [favPhotos, setFavPhotos] = useState([]);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [modalDetail, setModalDetail] = useState();
-  //const [favFlag, setFavFlag] = useState(false);
-  //handles the array that keeps track of the favorite photo
-  // const handleFavList = (selected, photo) => {
-  //   if (selected) {
-  //     setFavPhotos((prevFavPhotos) => {
-  //       const updatedFavPhotos = [...prevFavPhotos, photo.id];
-
-  //       // console.log("fav photo list:", updatedFavPhotos);
-  //       return updatedFavPhotos;
-  //     });
-  //   } else {
-  //     setFavPhotos((prevFavPhotos) => {
-  //       const updatedFavPhotos = prevFavPhotos.filter(
-  //         (valId) => valId !== photo.id
-  //       );
-  //       //console.log("fav photo list:", updatedFavPhotos);
-  //       return updatedFavPhotos;
-  //     });
-  //   }
-  // };
-
-  // //handles the favbadge icon on the nav bar
-  // const handleFavListFlag = () => {
-  //   return favPhotos.length ? true : false;
-  // };
-
-  // const handleModal = (photo) => {
-  //   const isModalOpenNew = !isModalOpen;
-  //   setIsModalOpen(isModalOpenNew);
-  //   setModalDetail(photo);
-  // };
+  const { state, handleFavList, handleFavListFlag, handleModal } =
+    useApplicationData();
 
   return (
     <div className="App">
--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,30 +1,44 @@
-export const handleFavList = (selected, photo) => {
-  if (selected) {
-    setFavPhotos((prevFavPhotos) => {
-      const updatedFavPhotos = [...prevFavPhotos, photo.id];
+import { useState } from "react";
 
-      // console.log("fav photo list:", updatedFavPhotos);
-      return updatedFavPhotos;
-    });
-  } else {
-    setFavPhotos((prevFavPhotos) => {
-      const updatedFavPhotos = prevFavPhotos.filter(
-        (valId) => valId !== photo.id
-      );
-      //console.log("fav photo list:", updatedFavPhotos);
-      return updatedFavPhotos;
-    });
-  }
+const useApplicationData = () => {
+  const [favPhotos, setFavPhotos] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalDetail, setModalDetail] = useState();
+  const handleFavList = (selected, photo) => {
+    if (selected) {
+      setFavPhotos((prevFavPhotos) => {
+        const updatedFavPhotos = [...prevFavPhotos, photo.id];
+
+        // console.log("fav photo list:", updatedFavPhotos);
+        return updatedFavPhotos;
+      });
+    } else {
+      setFavPhotos((prevFavPhotos) => {
+        const updatedFavPhotos = prevFavPhotos.filter(
+          (valId) => valId !== photo.id
+        );
+        //console.log("fav photo list:", updatedFavPhotos);
+        return updatedFavPhotos;
+      });
+    }
+  };
+
+  //handles the favbadge icon on the nav bar
+  const handleFavListFlag = () => {
+    return favPhotos.length ? true : false;
+  };
+
+  const handleModal = (photo) => {
+    const isModalOpenNew = !isModalOpen;
+    setIsModalOpen(isModalOpenNew);
+    setModalDetail(photo);
+  };
+  return {
+    state: { favPhotos, isModalOpen, modalDetail },
+    handleFavList,
+    handleFavListFlag,
+    handleModal,
+  };
 };
 
-//handles the favbadge icon on the nav bar
-export const handleFavListFlag = () => {
-  return favPhotos.length ? true : false;
-};
-
-export const handleModal = (photo) => {
-  const isModalOpenNew = !isModalOpen;
-  setIsModalOpen(isModalOpenNew);
-  setModalDetail(photo);
-};
-
+export default useApplicationData;
