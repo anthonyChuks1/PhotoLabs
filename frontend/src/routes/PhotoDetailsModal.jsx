c566e7c80085c9b0ac62e9162e6a593e181e1af7
anthonyChuks1 <anuebunwaanthony1@gmail.com>
2024-10-15 13:50:57 -0400

    minor adjustments

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -5,7 +5,12 @@ import closeSymbol from "../assets/closeSymbol.svg";
 import PhotoFavButton from "components/PhotoFavButton";
 import PhotoList from "components/PhotoList";
 
-const PhotoDetailsModal = ({ handleModal, photo, favPhotos, handleFavList}) => {
+const PhotoDetailsModal = ({
+  handleModal,
+  photo,
+  favPhotos,
+  handleFavList,
+}) => {
   const {
     urls: { full, regular } = {},
     user: { id, profile, username, name } = {},
@@ -15,42 +20,62 @@ const PhotoDetailsModal = ({ handleModal, photo, favPhotos, handleFavList}) => {
 
   const similar_photos_array = Object.values(similar_photos).flat();
 
- 
-  
   return (
     <div className="photo-details-modal">
-      <button
-        className="photo-details-modal__close-button"
-        onClick={handleModal}
-      >
-        <img src={closeSymbol} alt="close symbol" />
-      </button>
-      <div>
-      <PhotoFavButton photoId={id} photo = {photo} favPhotos={favPhotos} handleFavList={handleFavList}/>
+  {/* Top Close Button */}
+  <div>
+    <button
+      className="photo-details-modal__close-button"
+      onClick={handleModal}
+    >
+      <img src={closeSymbol} alt="close symbol" />
+    </button>
+  </div>
 
-        <img className="photo-details-modal__image" src={full} />
-        <div className="photo-details-modal__photographer-details">
-          <img
-            src={profile}
-            className="photo-details-modal__photographer-profile"
-          />
-          <div className="photo-details-modal__photographer-info">
-            <p>{name}</p>
-            <p className="photo-details-modal__photographer-location">
-              {city}, {country}
-            </p>
-          </div>
+  {/* Image Section */}
+  <div className="photo-details-modal__image">
+    <PhotoFavButton
+      photoId={id}
+      photo={photo}
+      favPhotos={favPhotos}
+      handleFavList={handleFavList}
+    />
+    <img src={full} alt="Selected photo" />
+  </div>
 
-          <div></div>
-        </div>
-        <p className="photo-details-modal__header ">Similar Photos</p>
-        <div>
-          <div className="photo-details-modal__images">
-            <PhotoList photos={similar_photos_array} favourites={favPhotos} handleFavList={handleFavList}/>
-          </div>
-        </div>
-      </div>
+  {/* Photographer Details */}
+  <div className="photo-details-modal__photographer-details">
+    <img
+      src={profile}
+      className="photo-details-modal__photographer-profile"
+      alt="Photographer profile"
+    />
+    <div className="photo-details-modal__photographer-info">
+      <p>{name}</p>
+      <p className="photo-details-modal__photographer-location">
+        {city}, {country}
+      </p>
     </div>
+  </div>
+
+  {/* Top Bar for Flex Layout */}
+  <div className="photo-details-modal__top-bar"></div>
+
+  {/* Related Photos Section */}
+  <div>
+    <div className="photo-details-modal__images">
+      <p className="photo-details-modal__header">
+        Related Photos
+      </p>
+      <PhotoList
+        photos={similar_photos_array}
+        favourites={favPhotos}
+        handleFavList={handleFavList}
+      />
+    </div>
+  </div>
+</div>
+
   );
 };
 
